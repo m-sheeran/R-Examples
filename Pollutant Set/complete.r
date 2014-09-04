04a90190d2d11d65fb8851572da11a4e691f0219
m-sheeran <sheeran.morgan@gmail.com>
2014-09-03 19:49:27 -0700

    Update complete.r

--- Pollutant Set/complete.r
+++ Pollutant Set/complete.r
@@ -3,32 +3,32 @@
 
 
 complete = function(directory, id = 1:332)
+{
+  # Enumerate files and create empty data frames
+  dat = list.files(directory, full.names = TRUE)
+  data = data.frame()
+  nobs.frame = data.frame()
+  id.frame = data.frame()
+  complete.frame = data.frame()
+    
+  for (i in id)
   {
-    # Enumerate files and create empty data frames
-    dat = list.files(directory, full.names = TRUE)
-    data = data.frame()
-    nobs.frame = data.frame()
-    id.frame = data.frame()
-    complete.frame = data.frame()
+    data = read.csv(dat[i])
     
-    for (i in id)
-      {
-        data = read.csv(dat[i])
-    
-        # Fill id.frame with associated file id's
-        monitor.id = i
-        id.frame = rbind(id.frame, monitor.id)
-        colnames(id.frame) = "id"
+    # Fill id.frame with associated file id's
+    monitor.id = i
+    id.frame = rbind(id.frame, monitor.id)
+    colnames(id.frame) = "id"
       
-        # Sum total complete cases per monitor id
-        comp= complete.cases(data)
-        nobs = sum(comp)
-        nobs.frame = rbind(nobs.frame, nobs)
-        colnames(nobs.frame) = "nobs"
-      }
-    
-    # Column merge both id and observation dataframes
-    complete.frame = cbind(id.frame, nobs.frame)
-    complete.frame
-    
+    # Sum total complete cases per monitor id
+    comp= complete.cases(data)
+    nobs = sum(comp)
+    nobs.frame = rbind(nobs.frame, nobs)
+    colnames(nobs.frame) = "nobs"
   }
+    
+  # Column merge both id and observation dataframes
+  complete.frame = cbind(id.frame, nobs.frame)
+  complete.frame
+    
+}
