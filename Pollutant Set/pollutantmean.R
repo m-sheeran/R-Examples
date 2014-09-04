3acd0b1f06af16db33b433182129daef42d28581
m-sheeran <sheeran.morgan@gmail.com>
2014-09-03 19:48:12 -0700

    Update pollutantmean.R

--- Pollutant Set/pollutantmean.R
+++ Pollutant Set/pollutantmean.R
@@ -2,26 +2,26 @@
 ## a specified list of monitors                                       ##
 
 pollutantmean = function (directory, pollutant, id = 1:332)
-  {
+{
    
-    dat = list.files(directory, full.names = TRUE)
-    data = data.frame()
+  dat = list.files(directory, full.names = TRUE)
+  data = data.frame()
     
-    ## Fill empty data frame with data from id selection
-    for (i in id)
-      {
-        data = rbind(data, read.csv(dat[i]))
+  ## Fill empty data frame with data from id selection
+  for (i in id)
+  {
+    data = rbind(data, read.csv(dat[i]))
               
-      }         
+  }         
       
-    # Subset data for specified pollutant
-    pol_subset = data[ , pollutant]
+  # Subset data for specified pollutant
+   pol_subset = data[ , pollutant]
    
  
-    ## Calculate pollutant mean from data subset
-    pol_mean = mean(pol_subset, na.rm = TRUE)
+  ## Calculate pollutant mean from data subset
+  pol_mean = mean(pol_subset, na.rm = TRUE)
   
-    ## Return pollutant mean
-    pol_mean
+  ## Return pollutant mean
+  pol_mean
   
-  }
+}
