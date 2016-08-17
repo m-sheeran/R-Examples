f4c34eb0b7f63df4cc523518d0a321ef5a8250e6
m-sheeran <sheeran.morgan@gmail.com>
2016-08-17 09:03:56 -0700

    Update sku_frag.R
    Noticed some multi-SKU locations were still showing up despite having zero inventory due to Inventory team not clearing the location in the WMS. Updated script to accommodate for this and used the bind_rows/bind_cols functions from dplyr instead of rbind/cbind from base R.
    
    Minor aesthetic changes to code layout. Updated description of script to clarify that while SKU fragmentation occurs, the script serves as measure of impact on pick mod.

--- sku_frag.R
+++ sku_frag.R
@@ -1,6 +1,6 @@
 # Determine the total SKU fragmentation in Active Pick Mod. We define fragmentation as the same SKU being in multiple Active Pick 
 # locations. Sometimes this is by design, e.g., slotting the same backpack or shoe into multiple carton flow locations in order to 
-# absorb the impact on the pick mod. More often than not, we run into instances of the same SKU being in 15 different shelving locs.
+# absorb the impact on the pick mod. This is to measure impact of fragmentation in pick mod.
 
 # Additionally, we break out the fragmentation measure to account for two scenarios - With multi-SKU locations included, and without
 # including multi-SKU locations, by filtering off the determination zones.
@@ -11,29 +11,45 @@ sku_frag = function(activeLocations)
   require(data.table)
   require(dplyr)
   
-  #read in all Active Pick locations
+  # Read in all Active Pick locations
   totalLocations = fread(activeLocations)
   
-  #select in only data we want from CSV
-  totalLocations = totalLocations %>% select(SKU:DET_ZONE)
+  # Select in only data we want from CSV
+  totalLocations = totalLocations %>% 
+                   select(SKU:DET_ZONE)
   
   #Excluding multi-SKU locations
-  nomulti = totalLocations %>% filter(DET_ZONE != 'RTN') %>% summarise(total.locations = n(), distinct.sku = n_distinct(SKU))
+  nomulti = totalLocations %>% 
+            filter(DET_ZONE != 'RTN') %>% 
+            summarise(total.locations = n(), distinct.sku = n_distinct(SKU))
   
   
   
+  # Reduce Multi-SKU locations to only those with non-zero inventory, combine with non-Multi-SKU locations
+  # then summarize for total fragmentation
   
-  multi = totalLocations %>% summarise(total.locations = n(), distinct.sku = n_distinct(SKU))
+  multi_non_zero = totalLocations %>%
+                   filter(DET_ZONE == 'RTN' & UNITS != 0)
+  
+  shf_locs = totalLocations %>%
+             filter(DET_ZONE != 'RTN')
+  
+  multi = bind_rows(multi_non_zero, shf_locs) 
+  
+  multi = multi %>%
+          summarise(total.locations = n(), distinct.sku = n_distinct(SKU))
+
   
   
-  #First row is without multi-SKU locations, second row is with multi-SKU locations
-  totalFrag = rbind(nomulti, multi)  
+  # First row is without multi-SKU locations, second row is with multi-SKU locations
+  totalFrag = bind_rows(nomulti, multi)
   
-  totalFrag = totalFrag %>% mutate(fragmentation = 1 - (distinct.sku / total.locations))
+  totalFrag = totalFrag %>% 
+              mutate(fragmentation = 1 - (distinct.sku / total.locations))
   
   frag_type = data.table(type = c("No multi-SKU", "Multi-SKU"))
   
-  totalFrag = cbind(frag_type, totalFrag)
+  totalFrag = bind_cols(frag_type, totalFrag)
   
   return(totalFrag)
 }
